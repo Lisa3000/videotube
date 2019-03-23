859ca2a67660e198b8c835f1f202d9644d3ebb60
Lisa <lisabeth.nel@gmail.com>
2019-03-23 08:40:49 -0700

    show video comments

--- includes/classes/CommentSection.php
+++ includes/classes/CommentSection.php
@@ -23,7 +23,11 @@ class CommentSection {
     $commentAction = "postComment(this, \"$postedBy\", $videoId, null, \"comments\")";
     $commentButton = ButtonProvider::createButton("COMMENT", null, $commentAction, "postComment");
 
-    // Get comments html
+    $comments = $this->video->getComments();
+    $commentItems = "";
+    foreach($comments as $comment) {
+      $commentItems .= $comment->create();
+    }
 
     return "<div class='commentSection'>
 
@@ -38,7 +42,7 @@ class CommentSection {
               </div>
 
               <div class='comments'>
-
+                $commentItems
               </div>
 
             </div>";
--- watch.php
+++ watch.php
@@ -2,7 +2,8 @@
 require_once("includes/header.php");  
 require_once("includes/classes/VideoPlayer.php"); 
 require_once("includes/classes/VideoInfoSection.php"); 
-require_once("includes/classes/CommentSection.php"); 
+require_once("includes/classes/Comment.php"); 
+require_once("includes/classes/CommentSection.php");
 
 if(!isset($_GET["id"])) {
   echo "Page not found";
