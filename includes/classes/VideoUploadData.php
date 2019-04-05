5054198e751aad1e9b8a58e81ece9f9ed8e9eaa6
Lisa <lisabeth.nel@gmail.com>
2019-04-05 11:00:59 -0700

    added edit video page

--- includes/classes/VideoUploadData.php
+++ includes/classes/VideoUploadData.php
@@ -17,5 +17,16 @@ class VideoUploadData {
       $this->category = $category;
       $this->uploadedBy = $uploadedBy;
   }
+
+  public function updateDetails($con, $videoId) {
+    $query = $con->prepare("UPDATE videos SET title=:title, description=:description, privacy=:privacy, category=:category WHERE id=:videoId");
+    $query->bindParam(":title", $this->title);
+    $query->bindParam(":description", $this->description);
+    $query->bindParam(":privacy", $this->privacy);
+    $query->bindParam(":category", $this->category);
+    $query->bindParam(":videoId", $videoId);
+
+    return $query->execute();
+  }
 }
 ?>
